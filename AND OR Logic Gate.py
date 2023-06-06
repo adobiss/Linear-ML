3bc73ac9343680f8697e04e3e3852e58d5056d5b
AD <95383833+adobiss@users.noreply.github.com>
2023-06-06 16:25:02 +0100

    update

--- AND OR Logic Gate.py
+++ AND OR Logic Gate.py
@@ -105,7 +105,7 @@ def plot_chart(X, w, attempt_counter):
    
 ## DATASET IMPORT
 
-dataset_path = 'D:/ML\Datasets/Logic Gates/AND_GATE.csv'
+dataset_path = 'D:/ML/Datasets/Logic Gates/AND_GATE.csv'
 dataset = np.genfromtxt(dataset_path, delimiter=',', skip_header=1)
 
 # Input values
