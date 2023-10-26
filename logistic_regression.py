5a36714c4196079d4e922074d37da77bbf63a93c
AD <95383833+adobiss@users.noreply.github.com>
2023-10-26 18:25:38 +0100

    Add feature scaling

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
 
 class Logit:
-    def __init__(self, batch_size=1, n_iters=100, learning_rate=1):
+    def __init__(self, batch_size=1, n_iters=10, learning_rate=1):
         """
         Constructor for the Logit class.
         Initializes parameters like learning rate, iterations, and batch size.
@@ -14,21 +15,15 @@ class Logit:
         self.activation_function = self._sigmoid_func
         self.loss_function = self._log_loss
         self.weights = None
-        self.loss = None
+        self.lin_output = None
 
-    def _min_max_scaling(self, X):
-        """
-        Scale features between 0 and 1 using Min-Max scaling.
-        """
-        col_max = np.max(X, axis=0)
-        col_min = np.min(X, axis=0)
-        return np.divide(X - col_min, col_max - col_min)
-    
     def _linear_output(self, X):
         """
         Compute the linear combination of features and weights.
         """
-        return np.matmul(X, self.weights)
+        self.lin_output = np.matmul(X, self.weights)
+        return self.lin_output
+        #return np.matmul(X, self.weights)
 
     def _sigmoid_func(self, x):
         """
@@ -43,7 +38,7 @@ class Logit:
         return -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
     
     def _compute_gradient(self, X, y, y_predicted):
-        gradient = np.mean(np.matmul(X.T, (y_predicted - y)))
+        gradient = np.matmul(X.T, (y_predicted - y)) / len(y_predicted)
         return gradient
     
     def predict(self, X):
@@ -51,7 +46,7 @@ class Logit:
         Predict the class labels for given features.
         """
         linear_output = self._linear_output(X)
-        y_predicted = self.activation_functionc(linear_output)
+        y_predicted = self.activation_function(linear_output)
         return y_predicted
     
     def fit(self, X, y):
@@ -64,9 +59,25 @@ class Logit:
         self.weights = np.zeros(n_features)
         
         for _ in range(self.n_iters):
-            linear_output = self._linear_output(X)
-            y_predicted = self.activation_function(linear_output)
-            self.loss = self.loss_function(y, y_predicted)
+            print(_)
+            y_predicted = self.predict(X)
+            lin_abs = np.abs(self.lin_output)
+            print('Linear output:', np.max(lin_abs))
+            print('Predictions:', y_predicted)
+            loss = self.loss_function(y, y_predicted)
+            print('Loss:', loss)
+            dw = self._compute_gradient(X, y, y_predicted)
+            print('Gradient:', dw)
+            self.weights -= self.lr * dw
+            print('Updated weights: ', self.weights)
+
+def min_max_scaling(X):
+        """
+        Scale features between 0 and 1 using Min-Max scaling.
+        """
+        col_max = np.max(X, axis=0)
+        col_min = np.min(X, axis=0)
+        return np.divide(X - col_min, col_max - col_min)
 
 # Read the dataset
 df = pd.read_csv(r"D:\ML\Portfolio\Projects\NumPy ML\Logistic Regression\grades_dataset.csv")
@@ -81,28 +92,76 @@ plt.scatter(df[26:].HoursStudied, df[26:].PreviousGrade, label='Passed')
 plt.xlabel('Hours Studied')
 plt.ylabel('Previous Grade')
 plt.legend(loc='best')
-#plt.show()
+plt.show()
 
-# Splitting the dataset
+# Split the dataset labels
 X = df.drop(labels='Passed', axis=1).to_numpy()
 y = df.Passed.to_numpy()
+
+# Feature scaling
+X_scaled = min_max_scaling(X)
+
+# Plotting the scaled data
+passed_mask = y == 1
+failed_mask = y == 0
+
+# Extracting values for students who passed and failed
+X_passed = X_scaled[passed_mask]
+X_failed = X_scaled[failed_mask]
+
+# Plotting
+plt.figure()
+plt.scatter(X_failed[:, 0], X_failed[:, 1], label='Failed', color='red')
+plt.scatter(X_passed[:, 0], X_passed[:, 1], label='Passed', color='green')
+plt.xlabel('Scaled Hours Studied')
+plt.ylabel('Scaled Previous Grade')
+plt.legend(loc='best')
+plt.title('Scaled Input Data')
+plt.show()
 
 # Append bias term to the input features
 n_samples, n_features = X.shape
 bias_inputs = np.ones((n_samples, 1))
 X = np.concatenate((X, bias_inputs), axis=1)
 
-# Splitting the dataset
+# Split the dataset
 seed = 5
 np.random.seed(seed)
-train_set = np.random.choice(len(X), round(len(X) * 0.6), replace=False)
-test_set = np.array(list(set(range(len(X))) - set(train_set)))
+train_set = np.random.choice(len(X_scaled), round(len(X_scaled) * 0.6), replace=False)
+test_set = np.array(list(set(range(len(X_scaled))) - set(train_set)))
 
-train_X = X[train_set]
+train_X = X_scaled[train_set]
 train_y = y[train_set]
-test_X = X[test_set]
+test_X = X_scaled[test_set]
 test_y = y[test_set]
 
 # Fit and print loss
 log_reg = Logit()
 log_reg.fit(train_X, train_y)
+pred = log_reg.predict(train_X)
+#print(pred)
+
+'''
+# Visualize the loss landscape for the composite function
+weight_range = np.linspace(-10, 10, 100)
+W1, W2 = np.meshgrid(weight_range, weight_range)
+
+Z = np.zeros(W1.shape)
+for i in range(W1.shape[0]):
+    for j in range(W1.shape[1]):
+        # a. Compute the linear output for the current weight values
+        linear_output = np.matmul(train_X[:,:2], np.array([W1[i,j], W2[i,j]]).T)
+        # b. Apply the activation function
+        predictions = log_reg._sigmoid_func(linear_output)
+        # c. Compute the loss
+        Z[i,j] = log_reg._log_loss(train_y, predictions)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(W1, W2, Z, cmap='viridis')
+ax.set_title('Loss Landscape for Composite Function')
+ax.set_xlabel('Weight 1')
+ax.set_ylabel('Weight 2')
+ax.set_zlabel('Loss')
+plt.show()
+'''
